e74b5d5e442bbb923e0f712849dd926a031ac688
Laciface <csomali18@gmail.com>
2020-08-10 14:33:51 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, request
+
+
+app = Flask('whack-a-mole')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+def main():
+    app.run(debug=True,
+            port=5000)
+
+
+if __name__ == '__main__':
+    main()
