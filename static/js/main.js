be8b2e9dbc5b41f09cee0db8b1e1fa39a420b390
Laciface <csomali18@gmail.com>
2020-08-11 10:20:18 +0200

    hammer added and moles random appearance

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,42 @@
-window.onload = molesMoveLeft
+// window.onload = molesMoveLeft
 
 function molesMoveLeft() {
     setTimeout(function () {
         let moles = document.querySelectorAll('.mole-pic')
-    for (let mole of moles){
-        mole.style.visibility = 'visible'
-    }
-    },3000)
+        for (let mole of moles) {
+            mole.style.visibility = 'visible'
+        }
+    }, 3000)
 }
+
+
+hammerThirdTry()
+
+function hammerThirdTry() {
+
+    let gameContainer = document.querySelector('.game');
+    gameContainer.onmousemove = applyHammerPointer
+
+    let hammer = document.querySelector('#hammer')
+
+    function applyHammerPointer(event) {
+        hammer.style.left = event.clientX - (hammer.offsetWidth / 3) + 'px'
+        hammer.style.top = event.clientY - (hammer.offsetHeight / 3) + 'px'
+    }
+}
+
+function randomMole() {
+    let moles = document.querySelectorAll('.mole-pic');
+    let moleId = Math.floor(Math.random() * 15) + 1
+    for (let mole of moles) {
+        if (parseInt(mole.parentNode.id) === moleId) {
+            mole.style.visibility = 'visible'
+            setTimeout(function () {
+                mole.style.visibility = 'hidden'
+                randomMole()
+            }, 2000)
+        }
+    }
+}
+
+randomMole()
